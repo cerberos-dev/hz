6613bb6bfef8ac2f8b346e1205a42a47630a4a86
Marius van Zundert <marius.vanzundert@youaredigital.nl>
2018-02-20 12:25:34 +0100

    added quarter model

---
+++ app/quarter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Quarter
+ * @package App
+ * @author Marius van Zundert <marius.vanzundert@youaredigital.nl>
+ */
+class Quarter extends Model
+{
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * @var bool
+     */
+    public $incrementing = false;
+}
