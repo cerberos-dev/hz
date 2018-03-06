9496186ab2020e0cf67719b3e163c9bcdad5a03b
Marius van Zundert <marius.vanzundert@youaredigital.nl>
2018-03-06 23:43:52 +0100

    Added database migration for credititems table

---
+++ database/migrations/2018_03_06_184522_add_credititems_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCredititemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credititems', function (Blueprint $table){
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('abbrev',10);
+            $table->string('name');
+            $table->unsignedDecimal('credits', 5, 2)->default(0);
+            $table->tinyInteger('type')->unsigned()->default(0);
+            $table->tinyInteger('status')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credititems');
+    }
+}
